4dce71805ee358667bbc569299327578eebbcfc2
Seba Fernandez <seba.fernandez95@gmail.com>
2019-01-15 21:44:53 -0300

    Primera prueba.
    Primer versión funcionando del programa.

--- libroCompras.py
+++ libroCompras.py
@@ -11,7 +11,10 @@ tipoComprobante = {'FCC':{'A':'001', 'B': '006', 'C' : '011'},
 compra = openpyxl.load_workbook ('Compras.xlsx')
 model  = openpyxl.load_workbook ('MODELO DE FORMULARIO.xlsx')
 
+#Hoja de COMPRAS
 sheetCompra = compra.active
+
+#Hoja del MODELO
 sheetModel = model.get_sheet_by_name('Carga de Datos')
 
 def codigoDeComprobante (tipo, varToTrim):
@@ -23,7 +26,7 @@ def codigoDeComprobante (tipo, varToTrim):
         elif (tipo is 'REC'):
             return tipoComprobante [tipo]['C']  
         else:
-            return 'XXX'
+            return '-----'
 
 def ptoDeVenta (varToTrim):
     return varToTrim [1:5].split("-")[0]
@@ -37,14 +40,28 @@ def cuit (idComprobante):
     auxString = idComprobante [:2] + idComprobante [3:11] + idComprobante [12:13]
     return auxString
 
+def calculoIVA (neto, impValor):
+    varNeto = float (neto)
+    varImpValor = float (impValor)
+
+    varResult = (varImpValor/varNeto) * 100
+
+    if (varResult == 21 or varResult == 27):
+        varResult = int (varResult)
+    else:
+        varResult = round (varResult,1)
+
+    return str (varResult)
+
 #Se resta 8 en el for porque la planilla de Compras tiene 8 filas semi-completas que no se usan.
 for i in range (sheetCompra.max_row-8):
-    
     #Copia fecha
     sheetModel.cell (row = 13+i, column = 1).value = sheetCompra.cell (row=2+i, column = 1).value
     aux = str (sheetCompra.cell (row=2+i, column = 2).value)
     aux2 = str (sheetCompra.cell (row=2+i, column = 3).value)
     aux3 = str (sheetCompra.cell (row=2+i, column = 4).value)
+    aux4 = str (sheetCompra.cell (row=2+i, column = 7).value)
+    aux5 = str (sheetCompra.cell (row=2+i, column = 8).value)
 
     #Copia comprobante, pto venta, nro comprobante, cuit, etc.
     sheetModel.cell (row = 13+i, column = 2).value = codigoDeComprobante (aux,aux2)
@@ -53,4 +70,16 @@ for i in range (sheetCompra.max_row-8):
     sheetModel.cell (row = 13+i, column = 7).value = cuit (aux3)
     sheetModel.cell (row = 13+i, column = 8).value = sheetCompra.cell (row=2+i, column = 6).value
 
+    #Copia valores de conceptos sobre importes; ergo, nros contables.
+    #Ordenado en cómo avanza de izq. a der. las columnas de COMPRAS.
+    sheetModel.cell (row = 13+i, column = 20).value = sheetCompra.cell (row=2+i, column = 7).value
+    sheetModel.cell (row = 13+i, column = 12).value = sheetCompra.cell (row=2+i, column = 9).value
+    sheetModel.cell (row = 13+i, column = 14).value = sheetCompra.cell (row=2+i, column = 10).value
+    sheetModel.cell (row = 13+i, column = 10).value = sheetCompra.cell (row=2+i, column = 11).value
+    sheetModel.cell (row = 13+i, column = 9).value = sheetCompra.cell (row=2+i, column = 12).value
+    try:
+        sheetModel.cell (row = 13+i, column = 21).value = calculoIVA (aux4,aux5)
+    except:
+        pass
+    
 model.save ('MODELO DE FORMULARIO.xlsx')
